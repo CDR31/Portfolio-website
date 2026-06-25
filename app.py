dc7988488f48e7d64c570121f799449610f3c65f
Pankaj Rajpoot <pankajraj2025434@gmail.com>
2026-06-25 20:39:45 +0530

    Final project updates before deployment

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
 mail = Mail(app)
